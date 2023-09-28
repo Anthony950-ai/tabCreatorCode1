a91c724f0d0edcf1e31ee7f9acdca72149930688
JonathanRivas457 <70966813+JonathanRivas457@users.noreply.github.com>
2023-09-28 15:24:02 -0700

    Add files via upload
    Program shows dominant frequencies at a given time

--- main.py
+++ main.py
@@ -1,54 +1,79 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
+from scipy.interpolate import interp1d
 
-#load data
+with open("C:/Users/Mario/Desktop/Audios/audios.txt" , 'r') as file:
+    content = file.read()
+    words = content.split()
 
-dataset = np.loadtxt('filename', delimeter = ',')
 
-#input values
-x = dataset[:, 0:8]
-#output values
-y = dataset[:, 8]
+n_fft = 1024  # Window size
+hop_length = 256  # Hop size (overlap)
 
-x = torch.tensor(x, dtype = torch.float32)
-y = torch.tensor(y, dtype = torch.float32).reshape(-1,1)
+#list that holds all frequencies
+allFrequencies = []
 
-#create model MOFDIFY LAYERS AS NEEDED
+for af in words:
+    # Load an audio file
+    audio_file = "C:/Users/Mario/Desktop/Audios/" + af
+    y, sr = librosa.load(audio_file)
 
-model = nn.Sequential(nn.Linear(8, 12),
-                      nn.ReLU(),
-                      nn.Linear(12, 8),
-                      nn.ReLU(),
-                      nn.Linear(8, 1),
-                      nn.Sigmoid())
+    # Calculate the STFT
+    n_fft = 1024  # Window size
+    hop_length = 256  # Hop size (overlap)
+    D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length)
 
-#create loss and optimization functions
-loss_fn = nn.CrossEntropyLoss
-optimizer = optim.Adam(model.parameters(), lr = 0.001)
+    # Calculate the magnitude spectrogram
+    magnitude_spec = np.abs(D)
 
-#establish target batches and epocs
-n_epochs = 100
-batch_size = 10
+    # Calculate the center frequencies manually
+    freqs = np.fft.fftfreq(n_fft, 1 / sr)
+    freqs = freqs[:n_fft // 2]  # Keep only positive frequencies
 
-for epoch in range(n_epochs):
-    for i in range(0, len(x), batch_size):
-        Xbatch = x[i:i + batch_size]
-        y_pred = model(Xbatch)
-        ybatch = y[i:i + batch_size]
-        loss = loss_fn(y_pred, ybatch)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f'Finished epoch {epoch}, latest loss {loss}')
+    # Define the target time in seconds (e.g., 0.5 seconds)
+    target_time = 1.0
 
-# compute accuracy (no_grad is optional)
-with torch.no_grad():
-    y_pred = model(x)
+    # Find the index of the time frame closest to the target time
+    target_frame_index = int(np.round(target_time * sr / hop_length))
 
-accuracy = (y_pred.round() == y).float().mean()
-print(f"Accuracy {accuracy}")
+    # Get the frequencies and magnitudes for the target time frame
+    target_frame = magnitude_spec[:, target_frame_index]
+
+    # Find the 5 most prevalent frequencies at the target time frame
+    num_top_frequencies = 7
+    top_indices = np.argsort(target_frame)[-num_top_frequencies:]
+    top_frequencies = freqs[top_indices]
+    top_magnitudes = target_frame[top_indices]
+
+
+    # Print the n most prevalent frequencies and their magnitudes at the target time frame
+    print("For " + af)
+    for i in range(num_top_frequencies):
+        print(f"Top Frequency {num_top_frequencies - i}: {top_frequencies[i]} Hz (Magnitude: {top_magnitudes[i]})")
+        allFrequencies.append(top_frequencies[i])
+
+    print("\n")
+with open('data.txt', 'w') as file:
+    for i in range(len(allFrequencies)):
+        if(i % num_top_frequencies == 0) or (i == 0):
+            file.write(str(allFrequencies[i]) + ",")
+        else:
+            file.write(str(allFrequencies[i]) + "\n")
+
+
+
+
+    #plt.figure(figsize=(10, 6))
+    #librosa.display.specshow(librosa.amplitude_to_db(magnitude_spec, ref=np.max), sr=sr, hop_length=hop_length,
+    #                      x_axis='time', y_axis='hz')
+    # plt.colorbar(format='%+2.0f dB')
+    #plt.title('Spectrogram')
+    # plt.show()
+
+    #input("continue")
+
 
 
 
